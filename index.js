ab4a0566bb9168c7d86712bc3bb2150da21b0e80
fuz1700 <fuz77it@gmail.com>
2024-10-28 13:57:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let bal = prompt ("balini yoz")
+if( bal < 70 ) {
+    alert("kirolmadin")
+} else if ( bal >= 70 && bal<= 80 ) {
+    alert("supper kontrakt")
+}else if ( bal >= 80 && bal <= 90 ) {
+    alert("kontrakt kirdin")
+}if ( bal > 90 ) {
+    alert ("grand kirdin")
+}
+else {
+    alert("error")
+}
